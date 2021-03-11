86d88caa6c9a9daf4fb927955c2f0f4b9969606b
Viktoria <miheykoviktoria@yandex.ru>
2021-03-11 20:34:16 +0300

    Compile my_trial_project

--- OpenGL_Practice_FULL-template/src/main.cpp
+++ OpenGL_Practice_FULL-template/src/main.cpp
@@ -40,19 +40,26 @@ void glfwErrorCallback(int error, const char* description) {
 
 void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     // Выходим по нажатию Escape
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
     }
     // по пробелу включаем или выключаем вращение автоматом
     if (key == GLFW_KEY_SPACE && action == GLFW_PRESS){
     }
 }
 
-void glfwMouseButtonCallback(GLFWwindow* window, int button, int state, int mod) {
+void glfwMouseButtonCallback(GLFWwindow* window, int button, int state, int mod)
+{
     // обработка левой кнопки
-    if(button == GLFW_MOUSE_BUTTON_1){
-        if(state == GLFW_PRESS){
+    if(button == GLFW_MOUSE_BUTTON_1)
+    {
+        if (state == GLFW_PRESS)
+        {
             leftButtonPressed = true;
-        }else{
+        }
+        else
+        {
             leftButtonPressed = false;
         }
     }
@@ -111,12 +118,13 @@ int main(int argc, char *argv[]) {
 #endif
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "The rotating cube!", NULL, NULL);
     if (!window) {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
 
+    //делаем текущее окно активным, выполняем контекст OpenGL для него
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);        // вертикальная синхронизация
     CHECK_GL_ERRORS();
@@ -211,7 +219,7 @@ int main(int argc, char *argv[]) {
         glGenTextures(1, &textureId);
         glBindTexture(GL_TEXTURE_2D, textureId);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,              // формат внутри OpenGL
-                     info.width, info.height, 0,            // ширинна, высота, границы
+                     info.width, info.height, 0,            // ширина, высота, границы
                      info.withAlpha ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, info.data); // формат входных данных
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
